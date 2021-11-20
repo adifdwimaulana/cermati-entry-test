9c624c30c25a212178c5c51af8c083c9505d32df
adifdwimaulana <adif.maulana@thebonza.com>
2021-11-21 05:01:08 +0700

    feat: add newsletter panel and it's behavior

--- main.js
+++ main.js
@@ -3,8 +3,7 @@ document.getElementById("year").innerHTML = year;
 
 const cookie = document.getElementById("cookie");
 const hero = document.getElementById("hero");
-const highlight = document.getElementById("highlight");
-const footer = document.getElementsByName("footer");
+const newsletter = document.getElementById("newsletter");
 
 function hideCookie() {
   const height = cookie.offsetHeight;
@@ -12,3 +11,32 @@ function hideCookie() {
   cookie.style.transform = "translateY(-100%)";
   hero.style.marginTop = `-${height}px`;
 }
+
+function hideNewsletter() {
+  const height = newsletter.offsetHeight;
+  localStorage.setItem("closed_at", new Date());
+
+  newsletter.style.transform = "translateY(100%)";
+}
+
+function getScrollPercentage() {
+  return (
+    ((document.documentElement.scrollTop + document.body.scrollTop) /
+      (document.documentElement.scrollHeight -
+        document.documentElement.clientHeight)) *
+    100
+  );
+}
+
+window.onscroll = () => {
+  const closedAt = new Date(localStorage.getItem("closed_at"));
+  const now = new Date();
+
+  const minutes = parseInt(
+    (Math.abs(now.getTime() - closedAt.getTime()) / (1000 * 60)) % 60
+  );
+
+  if (minutes >= 10 && getScrollPercentage() > 33.3) {
+    newsletter.style.transform = "translateY(0)";
+  }
+};
